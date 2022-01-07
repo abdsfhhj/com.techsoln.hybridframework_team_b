e938ce7d45b927e9d573ce89c25e1c19a7f82ae2
user <user@Dell-pc>
2022-01-07 10:47:05 +0530

    update porn xml with Testng libarary

---
+++ com.techsoln.hybridframework_team_b/src/test/java/com/techsoln/hybridframework_TestBase/Demo.java
@@ -0,0 +1,10 @@
+package com.techsoln.hybridframework_TestBase;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("good Evening");
+	}
+
+}
